0f18544a80043700781544478bd3264cc992ee58
francescogradi <francescogradi@gmail.com>
2019-06-13 12:44:14 +0200

    Nuovo file hadoop con query e tabelle aggiornabili

--- hadoopSrc/TweetSentiment.java
+++ hadoopSrc/TweetSentiment.java
@@ -1,12 +1,12 @@
 import java.io.IOException;
 import java.util.StringTokenizer;
 import java.util.ArrayList;
+import java.util.List;
 import java.io.FileReader;
 import java.io.FileInputStream;
 import java.util.Date;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-
 
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.conf.Configuration;
@@ -21,6 +21,8 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import static org.apache.hadoop.hbase.util.Bytes.toBytes;
 
@@ -34,6 +36,10 @@ import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -41,6 +47,8 @@ import org.json.simple.parser.ParseException;
 
 
 public class TweetSentiment extends Configured implements Tool {
+
+    public static final Log logt = LogFactory.getLog(TweetSentiment.class);
 
     public static Double getScore(ArrayList<Double> sentiments) {
 
@@ -60,10 +68,21 @@ public class TweetSentiment extends Configured implements Tool {
     public static float parseString (String s, JSONObject jsonObjectIt, JSONObject jsonObjectEng){
 
         // Suddivide la frase in parole ed elimina eventuale punteggiatura
+
         String[] words = s.split("\\s+");
         for (int i = 0; i < words.length; i++) {
             words[i] = words[i].replaceAll("[^\\w]", "");
+
+            logt.info("Confronto: " + words[i] + " con " + QUERY);
+
+            if (words[i].equalsIgnoreCase(QUERY)) {
+                THEREISQUERY = true;
+                logt.info("Ho trovato una parola ");
+            }
         }
+
+        if (THEREISQUERY == false)
+            return 0;
 
         float score = 0;
         ArrayList<Double> sentiments = null;
@@ -81,11 +100,11 @@ public class TweetSentiment extends Configured implements Tool {
         }
 
         return score;
-
     }
 
-
     public static class ClassificatorMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+        public static final Log logm = LogFactory.getLog(ClassificatorMapper.class);
 
         private int score = 0;
 
@@ -117,19 +136,39 @@ public class TweetSentiment extends Configured implements Tool {
             if (score > 0 && score < 3)
                 classification = new Text("Positive");
             if (score >= 3)
-                classification = new Text("Very Positive");
+                classification = new Text("VeryPositive");
 
             if (score < 0 && score > -3)
                 classification = new Text("Negative");
             if (score <= -3)
-                classification = new Text("Very Negative");
+                classification = new Text("VeryNegative");
             
-            context.write(classification, new IntWritable(1));
-            
+            if (THEREISQUERY == true) {
+                logm.info("classificazione " + classification +  " 1");
+                context.write(classification, new IntWritable(1));
+                THEREISQUERY = false;
+            }
+            else {
+                context.write(classification, new IntWritable(0));
             }
         }
+    }
 
     public static class ClassificationCounterReducer extends TableReducer<Text,IntWritable,Text> {
+
+        public static final Log log = LogFactory.getLog(ClassificationCounterReducer.class);
+
+        
+        private HTable hTable;
+        protected void setup(Context context) throws IOException, InterruptedException {
+            try {
+                hTable = new HTable(configuration, QUERY);
+            }
+            catch (IOException e) {
+                System.out.printf("Error getting table from HBase", e);
+            }
+        }   
+        
 
         public void reduce(Text classification, Iterable<IntWritable> values, Context context)
                 throws IOException, InterruptedException {
@@ -137,20 +176,41 @@ public class TweetSentiment extends Configured implements Tool {
             for (IntWritable val : values) {
                 sum += val.get();
             }
+            
+            // Leggo dal database prima di aggiornare la tabella
+            Get g = new Get(Bytes.toBytes(classification.toString()));
+            Result resultTable = hTable.get(g);
+
+            byte [] oldByteValue = resultTable.getValue(Bytes.toBytes("number"),Bytes.toBytes("value"));
+            
+            int oldValue = Integer.parseInt(Bytes.toString(oldByteValue));
+
+            log.info("Valore somma " + sum + " " + oldValue);
+
+            sum += oldValue;
 
             Put put = new Put(Bytes.toBytes(classification.toString()));
-
             String result = Integer.toString(sum);
 
             put.addColumn( Bytes.toBytes("number"), Bytes.toBytes("value"), Bytes.toBytes(result) );
 
             context.write(classification, put);
         }
+        
     }
 
     private static String OUTPUT_TABLE = "sentiment";
+    private static String QUERY;
+    private static boolean THEREISQUERY = false;
+    private static Configuration configuration;
+    //private static HTable hTable;
 
     public int run(String[] args) throws Exception  {
+
+        if (args.length > 1)
+            QUERY = args[1];
+
+        configuration = getConf();
 
         Job job = Job.getInstance(getConf(), "Sentiment Count");
 
@@ -165,17 +225,19 @@ public class TweetSentiment extends Configured implements Tool {
 
         HBaseAdmin admin = new HBaseAdmin(getConf());
 
-        HTableDescriptor tableDescriptor = new HTableDescriptor(TableName.valueOf(OUTPUT_TABLE + "_" + now));
+        HTableDescriptor tableDescriptor = new HTableDescriptor(TableName.valueOf(QUERY));
 
         tableDescriptor.addFamily(new HColumnDescriptor("number"));
 
-        if (!admin.tableExists(TableName.valueOf(OUTPUT_TABLE + "_" + now)))
-            admin.createTable(tableDescriptor);
+        if (!admin.tableExists(TableName.valueOf(QUERY)))
+           admin.createTable(tableDescriptor);
+        
+        //hTable = new HTable(getConf(), QUERY);
 
         TableMapReduceUtil.initTableReducerJob(
-                OUTPUT_TABLE + "_" + now,
-                TweetSentiment.ClassificationCounterReducer.class,
-                job);
+            QUERY,
+            TweetSentiment.ClassificationCounterReducer.class,
+            job);
 
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(IntWritable.class);
